91536263366735cbe06e1d35b955e95acd4ba7f4
NameForSimon <565587783@qq.com>
2018-10-25 16:42:17 +0800

    已更新springboot文件的上传与下载

--- src/main/java/com/wndz/application/HelloController.java
+++ src/main/java/com/wndz/application/HelloController.java
@@ -1,12 +1,25 @@
 package com.wndz.application;
 
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Date;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.wndz.bean.Wndz;
 
@@ -38,4 +51,82 @@ public class HelloController {
 		wndz.date=new Date();
 		return wndz;
 	}
+	
+	@RequestMapping("/helloworld4")
+	public String hello4() {
+		return "upload";
+	}
+	
+	@RequestMapping("/upload")
+	public String upload(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
+		 if (!file.isEmpty()) {
+	            String saveFileName = file.getOriginalFilename();
+	            File saveFile = new File(request.getSession().getServletContext().getRealPath("/upload/") + saveFileName);
+	            System.out.println("保存路径为："+saveFile);
+	            if (!saveFile.getParentFile().exists()) {
+	                saveFile.getParentFile().mkdirs();
+	            }
+	            try {
+	                BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(saveFile));
+	                out.write(file.getBytes());
+	                out.flush();
+	                out.close();
+	                return "success";
+	            } catch (FileNotFoundException e) {
+	                e.printStackTrace();
+	                return "上传失败";
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	                return "上传失败";
+	            }
+	        } else {
+	            return "上传失败";
+	        }
+	}
+	
+	@RequestMapping("/download")
+	public String download(HttpServletRequest request, HttpServletResponse response) {
+	String fileName = "background.JPG";// 设置文件名，根据业务需要替换成要下载的文件名
+    if (fileName != null) {
+        //设置文件路径
+        String realPath = "D:\\工作\\";
+        File file = new File(realPath , fileName);
+        if (file.exists()) {
+            response.setContentType("application/force-download");// 设置强制下载不打开
+            response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);// 设置文件名
+            byte[] buffer = new byte[1024];
+            FileInputStream fis = null;
+            BufferedInputStream bis = null;
+            try {
+                fis = new FileInputStream(file);
+                bis = new BufferedInputStream(fis);
+                OutputStream os = response.getOutputStream();
+                int i = bis.read(buffer);
+                while (i != -1) {
+                    os.write(buffer, 0, i);
+                    i = bis.read(buffer);
+                }
+                System.out.println("success");
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (bis != null) {
+                    try {
+                        bis.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (fis != null) {
+                    try {
+                        fis.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+    return null;
+}
 }
